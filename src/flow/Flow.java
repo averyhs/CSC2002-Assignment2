e4cc7658f844e4b0ed34ba854211f6b676021aa9
click-to-generate-name <k4s.txt@gmail.com>
2020-09-21 11:59:18 +0200

    Doc, comment, clean Flow and FlowPanel
    Part of cleaning is FlowPanel constructor now takes a String argument,
    FlowPanel is responsible for instantiating terrain, and provides Flow
    with some dimensions for the frame.

--- src/flow/Flow.java
+++ src/flow/Flow.java
@@ -1,59 +1,76 @@
 package flow;
 
 import javax.swing.*;
-
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 
+/**
+ * <p>GUI handler.</p>
+ * <p>Sets up a Frame with two panels. The display panel is 
+ * for a {@link FlowPanel} object and the control panel is 
+ * for buttons that the user can use to interact with the 
+ * simulation.</p>
+ * <p>This is a View class (of the MVC design pattern). It is
+ * responsible for the visual representation. It takes in input data
+ * and has action listeners for the buttons as well, but all processing
+ * is done by {@link FlowPanel}</p>
+ * 
+ * @author hrrhan002
+ */
 public class Flow {
-	static long startTime = 0;
+	// Frame dimensions
 	static int frameX;
 	static int frameY;
+
+	// FlowPanel -- controller
 	static FlowPanel fp;
 
-	// start timer
-	private static void tick(){
-		startTime = System.currentTimeMillis();
-	}
+	/**
+	 * <p>Set up the application GUI.</p>
+	 * <p>Components of the GUI are a frame, with a panel for {@link FlowPanel}
+	 * and a panel for buttons.</p>
+	 * 
+	 * @param frameX Width of frame
+	 * @param frameY Height of frame
+	 * @param landdata Terrain data to give to <code>FlowPanel</code>
+	 */
+	public static void setupGUI(String dataFile) {
 
-	// stop timer, return time elapsed in seconds
-	private static float tock(){
-		return (System.currentTimeMillis() - startTime) / 1000.0f; 
-	}
-
-	public static void setupGUI(int frameX,int frameY,Terrain landdata) {
-
-		// ** Window **
+		// =============
+		//  Frame setup
+		// =============
 		Dimension fsize = new Dimension(800, 800);
 		JFrame frame = new JFrame("Waterflow"); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout());
 
-		// ** Image panel **
+		// ============
+		//  Main panel
+		// ============
 		JPanel g = new JPanel();
 		g.setLayout(new BoxLayout(g, BoxLayout.PAGE_AXIS)); 
 
-		fp = new FlowPanel(landdata);
-		fp.setPreferredSize(new Dimension(frameX,frameY));
+		fp = new FlowPanel(dataFile);
+		frameX = fp.dimx();
+		frameY = fp.dimy();
+		fp.setPreferredSize(new Dimension(frameX, frameY));
 		g.add(fp);
 
-		// to do: add a MouseListener, buttons and ActionListeners on those buttons
+		// =========
+		//  Buttons
+		// =========
 
-		// ** Buttons **
-		
-		// "End" ends program when pressed
+		// "End" ends program
 		JButton endB = new JButton("End");
 		endB.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				fp.end();
 				frame.dispose();
-				// There's only one frame, so can just call exit
-				//System.exit(0);
 			}
 		});
-		
+
 		// "Reset" resets simulation
 		JButton resetB = new JButton("Reset");
 		resetB.addActionListener(new ActionListener() {
@@ -61,7 +78,7 @@ public class Flow {
 				fp.reset();
 			}
 		});
-		
+
 		// "Play" starts simulation
 		JButton playB = new JButton("Play");
 		playB.addActionListener(new ActionListener() {
@@ -69,7 +86,7 @@ public class Flow {
 				fp.play();
 			}
 		});
-		
+
 		// "Pause" pauses simulation
 		JButton pauseB = new JButton("Pause");
 		pauseB.addActionListener(new ActionListener() {
@@ -77,10 +94,13 @@ public class Flow {
 				fp.pause();
 			}
 		});
-		
-		// ** Button panel **
+
+		// ==============
+		//  Button panel
+		// ==============
 		JPanel b = new JPanel();
 		b.setLayout(new BoxLayout(b, BoxLayout.LINE_AXIS));
+
 		// Add buttons to panel
 		b.add(Box.createHorizontalGlue());
 		b.add(resetB);
@@ -91,39 +111,33 @@ public class Flow {
 		b.add(Box.createHorizontalGlue());
 		b.add(endB);
 		b.add(Box.createHorizontalGlue());
-		// Add panel to frame
+
 		g.add(b);
 
-		frame.setSize(frameX, frameY+50);	// a little extra space at the bottom for buttons
-		frame.setLocationRelativeTo(null);  // center window on screen
-		frame.add(g); //add contents to window
+		// =============
+		//  Frame setup
+		// =============
+		frame.setSize(frameX, frameY+50); // Extra space at the bottom for buttons
+		frame.setLocationRelativeTo(null);  // Center window on screen
+		frame.add(g); // Add components
 		frame.setContentPane(g);
 		frame.setVisible(true);
-		//Thread fpt = new Thread(fp);
-		//fpt.start();
 	}
 
-
+	/**
+	 * <p>Main method: read input file and invoke GUI.</p>
+	 * 
+	 * @param args Filepath to terrain data file
+	 */
 	public static void main(String[] args) {
-		Terrain landdata = new Terrain();
-
-		// check that number of command line arguments is correct
-		if(args.length != 1)
-		{
+		// Check that number of command line arguments is correct
+		if(args.length != 1) {
 			System.out.println("Incorrect number of command line arguments. Should have form: java -jar flow.java intputfilename");
 			System.exit(0);
 		}
 
-		// landscape information from file supplied as argument
-		// 
-		landdata.readData(args[0]);
-
-		frameX = landdata.getDimX();
-		frameY = landdata.getDimY();
-		SwingUtilities.invokeLater(()->setupGUI(frameX, frameY, landdata));
-
-		// to do: initialise and start simulation
-		
-		//System.out.println("fp: "+fp);
+		// Execute in event dispatch thread
+		SwingUtilities.invokeLater( () -> setupGUI(args[0]) );
 	}
+
 }
--- src/flow/FlowPanel.java
+++ src/flow/FlowPanel.java
@@ -1,88 +1,165 @@
 package flow;
 
-import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.Graphics;
-
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.awt.color.*;
 
+/**
+ * <p>Panel that manages the simulation.</p>
+ * 
+ * @author avk
+ */
 public class FlowPanel extends JPanel{
 
+	/* XXX: Not having this can result in unexpected InvalidClassExceptions 
+	 * due to sensitivity to different compilers according to documentation 
+	 * (see link), so i let Eclipse add a generated serialVersionUID
+	 * stackoverflow:
+	 * https://stackoverflow.com/questions/285793/what-is-a-serialversionuid-and-why-should-i-use-it
+	 */
+	private static final long serialVersionUID = 2900279500141540118L;
+
+	// ========
+	//  Fields
+	// ========
+
+	// Data
 	Grid grid;
 	Terrain terrain;
 	Water water;
-	
-	CyclicBarrier barrier;
-	
+
+	// Constants
 	final static int NUM_THREADS = 4;
 	final static int DROP_DEPTH = 1;
 	final static int DROP_SIZE = 3;
-	
+
+	// Count
 	AtomicInteger count;
 	JLabel countL;
-	
+
+	// Threading
+	CyclicBarrier barrier;
 	volatile boolean paused;
 	volatile boolean ended;
 
-	FlowPanel (Terrain t) {
-		this.setLayout(new FlowLayout(FlowLayout.RIGHT));
-		
-		terrain = t;
-		water = new Water (terrain);
-		grid = new Grid (NUM_THREADS, terrain.dimx, terrain.dimy);
+	// =============
+	//  Constructor
+	// =============
 
-		ended = false;
-		paused = true;
-		
-		count = new AtomicInteger(0);
-		
+	/**
+	 * <p><code>FlowPanel</code> constructor.</p>
+	 * <p>Initializes data, counter, threads, and mouse listener.</p>s
+	 * 
+	 * @param t Terrain object for simulation
+	 */
+	FlowPanel (String dataFile) {
+
+		// =========
+		//  Counter
+		// =========
+		// Determines where count label will be
+		this.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		// Label and properties
 		countL = new JLabel("0",10);
 		countL.setOpaque(true);
 		countL.setBackground(Color.lightGray);
 		countL.setBorder(BorderFactory.createEmptyBorder(1,2,1,2));
 		this.add(countL);
+		// Initialize counter
+		count = new AtomicInteger(0);
 
+		// ===================
+		//  Terrain and Water
+		// ===================
+		terrain = new Terrain();
+		terrain.readData(dataFile);
+		water = new Water (terrain);
+		grid = new Grid (NUM_THREADS, terrain.dimx, terrain.dimy);
+
+		// =========
+		//  Threads
+		// =========
+
+		// Set initial ended and paused states
+		ended = false;
+		paused = true;
+
+		// Set up a cyclic barrier
+		/* This defines the number of threads required to trip barrier
+		 * and what actions should be taken by the last thread to trip
+		 * the barrier (lambda expression is for Runnable interface)
+		 */
 		barrier = new CyclicBarrier(NUM_THREADS, () -> {
 			count.getAndIncrement();
 			countL.setText(String.valueOf(count.get()));
-			});
-		
-		// create and start threads
+		});
+
+		// Create and start threads
 		for (int s=0; s<NUM_THREADS; s++) {
 			Thread temp =  new Thread(new Simulate(s));
 			temp.start();
 		}
 
-		// Mouse listener adds water where user clicks
-		addMouseListener (new MouseAdapter() { 
-			public void mouseClicked (MouseEvent me) { 
+		// ================
+		//  Mouse listener
+		// ================
+		addMouseListener(new MouseAdapter() { 
+			public void mouseClicked(MouseEvent me) { 
 				try {
-					water.add (me.getX(), me.getY(), DROP_DEPTH, DROP_SIZE);
+					// Add water where user clicks
+					water.add(me.getX(), me.getY(), DROP_DEPTH, DROP_SIZE);
 					repaint();
 				}
 				catch (ArrayIndexOutOfBoundsException err) {} // Off map, do nothing
-			} 
+			}
 		});
 	}
 
-	// responsible for painting the terrain and water
-	// as images
+	// ============
+	//  Frame dims
+	// ============
+
+	/**
+	 * <p>Accessor for grid x-dimension.</p>
+	 * <p>Used to get frame dimensions for {@link Flow}.</p>
+	 * 
+	 * @return grid x-dimension
+	 */
+	int dimx() {
+		return grid.dimx();
+	}
+
+	/**
+	 * <p>Accessor for grid y-dimension.</p>
+	 * <p>Used to get frame dimensions for {@link Flow}.</p>
+	 * 
+	 * @return grid y-dimension
+	 */
+	int dimy() {
+		return grid.dimy();
+	}
+
+	// ==========
+	//  Painters
+	// ==========
+
+	/**
+	 * <p>Paints terrain and water.</p>
+	 * 
+	 * @param g Panel's graphics object
+	 */
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		// draw the landscape in greyscale as an image
+
+		// draw landscape (grayscale)
 		if (terrain.getImage() != null){
 			g.drawImage(terrain.getImage(), 0, 0, null);
 		}
@@ -92,105 +169,173 @@ public class FlowPanel extends JPanel{
 			g.drawImage(water.getImage(), 0, 0, null);
 		}
 	}
-	
+
+	/**
+	 * <p>Paints count label.</p>
+	 * 
+	 * @param g Panel's graphics object
+	 */
 	@Override
 	protected void paintChildren(Graphics g) {
 		super.paintChildren(g);
 	}
 
-	// controls
+	// ==========
+	//  Controls
+	// ==========
+
+	/**
+	 * <p>Resumes simulation.</p>
+	 * <p>Sets <code>paused</code> to false.</p>
+	 */
 	void play() {
 		paused = false;
 	}
-	
+
+	/**
+	 * <p>Pause simulation.</p>
+	 * <p>Sets <code>paused</code> to true.</p>
+	 */
 	void pause() {
 		paused = true;
-		System.out.println("-----------------------------------------------------------------------------------");
-		System.out.println("water added: "+water.waterAdded());
-		System.out.println("water removed: "+water.waterRemoved());
-		System.out.println("(water added) - (water removed): " + (water.waterAdded()-water.waterRemoved()));
-		System.out.println("water count: "+water.waterCount());
-		System.out.println("-----------------------------------------------------------------------------------");
+
+		// Water conservation testing    |
+		// Uncomment                     v
+		//System.out.println("+------------------------------------------------+");
+		//System.out.println(" water added: "+water.waterAdded());
+		//System.out.println(" water removed: "+water.waterRemoved());
+		//System.out.println(" (water added) - (water removed): " + (water.waterAdded()-water.waterRemoved()));
+		//System.out.println(" water count: "+water.waterCount());
+		//System.out.println("+------------------------------------------------+");
 	}
-	
+
+	/**
+	 * <p>Resets simulation.</p>
+	 * <p>Sets <code>paused</code> to true, resets water to zero,
+	 * resets counter to zero.</p>
+	 */
 	void reset() {
 		paused = true;
 		water.reset();
 		repaint();
-		
+
 		// Wait so threads don't rewrite countL
 		try { Thread.sleep(100); }
 		catch (InterruptedException err) { err.printStackTrace(); }
-		
+
 		count.set(0);
 		countL.setText("0");
 		repaint();
 	}
-	
+
+	/**
+	 * <p>Ends simulation.</p>
+	 * <p>Sets <code>ended</code> to true.</p>
+	 */
 	void end() {
 		ended = true;
 	}
 
+	/**
+	 * <p>Simulation engine.</p>
+	 * <p>Runnable class that is instantiated by {@link FlowPanel} 
+	 * as Threads to traverse the grid and simulate the flow of the 
+	 * water over the terrain. When a thread completes one iteration
+	 * (one traversal of it's section of the grid) it trips the 
+	 * cyclic barrier, so threads are synchronized at each 
+	 * traversal.</p>
+	 * <p>This is a Controller class (of the MVC design pattern).
+	 * It handles the data and makes decisions on what to do and
+	 * when to do it.</p>
+	 * 
+	 * @author avk
+	 */
 	class Simulate implements Runnable {
 
-		int tNum; // thread number
-		int lo, hi;
-		int[] curr, next; // coords of current pt and pt water goes to
+		int tNum; // Thread number [0, NUM_THREADS)
+		int[] coords; // Coords of current point
 
+		/**
+		 * <p><code>Simulate</code> constructor.</p>
+		 * @param t Thread number (0,1,...)
+		 */
 		Simulate (int t) {
 			tNum = t;
-
-			//lo = (int)(t*grid.dim()/NUM_THREADS);
-			//hi = (int)((t+1)*grid.dim()/NUM_THREADS);
-
-			curr = new int[2];
-			//next = new int[2];
+			coords = new int[2];
 		}
 
+		/**
+		 * <p>Traverses grid updating water depths.</p>
+		 */
 		@Override
 		public void run() {
 
-			while (!ended) {
+			while (!ended) { // Loop until simulation is ended
 
-				if (paused) {
+				if (paused) { // Just spin if paused
 					continue;
 				}
-				
-				for(int i=0; i<grid.subLen(); i++) {					
-					grid.getPermute(tNum, i, curr);
+
+				// Loop over a quarter of the grid (assuming 4 threads)
+				for(int i=0; i<grid.subLen(); i++) {
+					// Get coords of point to consider
+					grid.getPermute(tNum, i, coords);
 
 					if (onMapBoundary()) {
-						water.updateEdge(curr[0], curr[1]);
+						// Run off edge
+						water.updateEdge(coords[0], coords[1]);
 						repaint();
 					}
 
 					else if (onThreadBoundary()) {
-						water.updateS(curr[0], curr[1]);
+						// Check & transfer water with mutual exclusion
+						water.updateS(coords[0], coords[1]);
 						repaint();
 					}
 
 					else {
-						water.update(curr[0], curr[1]);
+						// Check & transfer water (no mutual exclusion)
+						water.update(coords[0], coords[1]);
 						repaint();
 					}
 				}
-				
+
 				try {
-					barrier.await();
+					barrier.await(); // Trip barrier
 				} catch (InterruptedException | BrokenBarrierException err) {
+					System.out.println("Error at cyclic barrier");
 					err.printStackTrace();
 				}
 			}
 		}
 
+		/**
+		 * <p>Determines if point is on the boundary of the map.</p>
+		 * @return true if point is on boundary, false otherwise
+		 */
 		boolean onMapBoundary() {
-			return curr[0]==0 || curr[1]==0 ||
-					curr[0]==grid.dimx()-1 || curr[1]==grid.dimy()-1;
+			// Min and max values of x and y
+			return coords[0]==0 || coords[1]==0 ||
+					coords[0]==grid.dimx()-1 || coords[1]==grid.dimy()-1;
 		}
 
+		/**
+		 * <p>Determines if point is in region on boundary of thread zones</p>
+		 * <p>I.e. if point is in a region where multiple threads will be accessing it.</p>
+		 * @return true if point is in boundary region, false otherwise
+		 */
 		boolean onThreadBoundary() {
-			int yBound = hi%grid.dimy();
-			return curr[1]==yBound || curr[1]==yBound-1 || curr[1]==yBound+1;
+			/* bound is the last row of the grid that the thread is responsible for.
+			 * This and the rows above and below it are accessed by multiple threads, 
+			 * so there needs to be mutual exclusion on that data.
+			 * 
+			 * Calculation of bound:
+			 * grid.dim()/NUM_THREADS divides grid into NUM_THREADS zones. Multiplying
+			 * by (tNum+1) selects the end of this thread's zone. %grid.dimy() gets the 
+			 * row number.
+			 */
+			int bound = ((tNum+1)*grid.dim()/NUM_THREADS)%grid.dimy();
+			return coords[1]==bound || coords[1]==bound-1 || coords[1]==bound+1;
 		}
 	} // End of Simulate class
 }
